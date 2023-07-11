2591caba3bddd36713e4f8b0141d4c5578c22559
Simon Köck <54739715+simonkoeck@users.noreply.github.com>
2023-07-11 15:14:59 +0200

    add version control

--- attack.sh
+++ attack.sh
@@ -9,6 +9,27 @@ fi
 url=$1
 target_dir=$(basename $url)
 
+github_user=simonkoeck
+github_repo=git-exposed
+
+# Path to this script
+script_path=$(realpath "$0")
+
+# Get the timestamp of the last commit to the repo
+github_time=$(curl --silent https://api.github.com/repos/$github_user/$github_repo/git/refs/heads/master \
+    | grep -Po '"date": "\K.*?(?=")' | head -1)
+
+# Convert the GitHub timestamp to seconds since the Unix epoch
+github_time_epoch=$(date --date="$github_time" +%s)
+
+# Get the timestamp of the last modification to this script and convert it to seconds since the Unix epoch
+script_time_epoch=$(stat -c %Y "$script_path")
+
+# If the GitHub version is newer, print a warning
+if [ "$github_time_epoch" -gt "$script_time_epoch" ]; then
+    echo "A newer version of this script is available. Please update your local version."
+fi
+
 # Check if .git directory is accessible
 if curl --output /dev/null --silent --head --fail "$url/.git"; then
     echo ".git directory found, starting download. this may take a while..."
